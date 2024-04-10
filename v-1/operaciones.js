fc5f5f57840057a0f98addb894743dd9c0bbfd11
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-10 18:06:55 +0200

    borrar digito individual

--- v-1/operaciones.js
+++ v-1/operaciones.js
@@ -4,7 +4,7 @@ var numero;
 var mostrarOperacion;
 var mostrarResultado;
 
-function cargarPagina(){
+function cargarPagina() {
     mostrarOperacion = document.getElementById('operacion');
     mostrarResultado = document.getElementById('resultado');
     operacion = '';
@@ -18,86 +18,101 @@ function cargarPagina(){
     document.getElementById('igual').addEventListener('click', igual);
     document.getElementById('borrar_actual').addEventListener('click', borrarActual);
     document.getElementById('borrar_todo').addEventListener('click', borrarTodo);
+    document.getElementById('borrar_uno').addEventListener('click', borrarUno);
     document.querySelectorAll('.numero').forEach(element => {
         element.addEventListener('click', setNumero);
-        
+
     });
-  
+
 }
 
-function sumar(){
-    if ( numero !== ''){
-    operar();
-    
+function sumar() {
+    if (numero !== '') {
+        operar();
+
     }
     operacion = '+';
     showOperacion();
-    
 
+
+}
+
+function borrarUno() {
+    if (operacion !== '=') {
+        numero = numero.toString();
+        if (numero.length > 1) {
+            numero = numero.substr(0, numero.length - 1);
+            numero = parseInt(numero);
+        } else{
+            numero = 0
+        }
+        showNumero();
+    } else {
+        mostrarOperacion.innerText = '';
+        operacion = '';
+    }
 }
 
 
 
+function restar() {
+    if (numero !== '') {
+        operar();
 
-
-function restar(){
-    if ( numero !== ''){
-    operar();
-    
     }
     operacion = '-'
-   showOperacion();
-    
+    showOperacion();
+
 }
 
-function multiplicar(){
-    if ( numero !== ''){
-    operar();
-    
+function multiplicar() {
+    if (numero !== '') {
+        operar();
+
     }
     operacion = "*"
-   showOperacion();
-    
+    showOperacion();
+
 }
 
-function dividir(){
-    if ( numero !== ''){
-    operar();
-    
-    
+function dividir() {
+    if (numero !== '') {
+        operar();
+
+
     }
     operacion = "/"
     showOperacion();
 }
 
-function borrarActual(){
+function borrarActual() {
     numero = 0;
     showNumero();
 }
 
-function borrarTodo(){
+function borrarTodo() {
     operacion = '';
     resultado = 0;
     numero = 0;
     mostrarOperacion.innerText = '';
     showNumero();
-    
+
 }
 
-function setNumero(e){
-    if (operacion == '='){
+function setNumero(e) {
+    if (operacion == '=') {
         numero = 0;
     }
     numero = numero + '' + this.value;
     numero = parseInt(numero);
     showNumero();
-    
+
 }
 
 
 
-function operar(){
-    switch(operacion){
+function operar() {
+    switch (operacion) {
         case '+':
             resultado += numero;
             showResultado();
@@ -109,11 +124,11 @@ function operar(){
             numero = '';
             break;
         case '/':
-            if (numero == 0){
+            if (numero == 0) {
                 mostrarResultado.innerText = 'No se puede dividir entre 0';
-            } else{
-            resultado /= numero;
-            showResultado();
+            } else {
+                resultado /= numero;
+                showResultado();
             }
             numero = '';
             break;
@@ -131,31 +146,31 @@ function operar(){
 }
 
 function igual() {
-    
-    if (operacion !== '' && operacion !== '='){
-    if (numero === ''){
-        numero = resultado;
-    }
-    mostrarOperacion.innerText = resultado + ' ' + operacion + ' ' + numero + '=';
-    operar();
-    operacion = '=';
+
+    if (operacion !== '' && operacion !== '=') {
+        if (numero === '') {
+            numero = resultado;
+        }
+        mostrarOperacion.innerText = resultado + ' ' + operacion + ' ' + numero + '=';
+        operar();
+        operacion = '=';
     }
 }
 
-function showNumero(){  
-    if (operacion === '='){
+function showNumero() {
+    if (operacion === '=') {
         resultado = 0;
         mostrarOperacion.innerText = '';
     }
     mostrarResultado.innerText = numero;
 }
-function showOperacion(){
+function showOperacion() {
     mostrarOperacion.innerText = resultado + ' ' + operacion;
 }
 
-function showResultado(){
+function showResultado() {
     mostrarResultado.innerText = resultado;
-    
+
 }
 
 window.addEventListener('load', cargarPagina)
